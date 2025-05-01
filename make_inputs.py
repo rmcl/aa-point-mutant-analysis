4544d3e7637e904996438574f4bddd46181831a9
Russell McLoughlin <rmcl@n0210.talapas.uoregon.edu>
2025-05-01 12:43:53 -0700

    fix make inputs.

--- make_inputs.py
+++ make_inputs.py
@@ -4,27 +4,37 @@ This is useful for generating input file that can be used by an array based SLUR
 """
 import json
 import sys
-from .constants import WILD_TYPE_STARTING_AMINO_ACIDS, ALL_AMINO_ACIDS
+import os
 
-if len(sys.argv) != 2:
-    print('Usage: python make_inputs.py <output_file_path>')
-    sys.exit(1)
-
-OUTPUT_FILE_PATH = sys.argv[1]
-
-already_done = set()
-with open(OUTPUT_FILE_PATH, 'r') as fp:
-    for l in fp:
-        data = json.loads(l)
-        already_done.add((data['residue_position'], data['mutated_aa']))
+from constants import WILD_TYPE_STARTING_AMINO_ACIDS, ALL_AMINO_ACIDS
 
 
-output_score_file_path = f'{OUTPUT_FILE_PATH}/inputs.jsonl'
-
-
-def generate_input_file():
+def main():
+    if len(sys.argv) != 2:
+        print('Usage: python make_inputs.py <output_file_path>')
+        sys.exit(1)
+    
+    OUTPUT_FILE_PATH = sys.argv[1]
+    output_score_file_path = f'{OUTPUT_FILE_PATH}/inputs.txt'
+    
+    os.makedirs(OUTPUT_FILE_PATH, exist_ok=True)
+    
+    already_done = set()
+    try:
+        with open(output_score_file_path, 'r') as fp:
+            for l in fp:
+                data = json.loads(l)
+                already_done.add((data['residue_position'], data['mutated_aa']))
+    except FileNotFoundError:
+        # score file exists so nothing has been done yet!
+        pass
+    
+    
     print('Generating input file...')
     with open(output_score_file_path, 'w+') as f:
         for residue_position in WILD_TYPE_STARTING_AMINO_ACIDS.keys():
             for mutated_aa in ALL_AMINO_ACIDS:
                 f.write(f'{residue_position},{mutated_aa}\n')
+
+if __name__ == '__main__':
+    main()
