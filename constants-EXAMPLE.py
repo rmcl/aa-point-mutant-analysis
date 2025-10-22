d5f34d56d9c9a08393cafded36f5ffa9444243ed
Russell McLoughlin <russ.mcl@gmail.com>
2025-10-22 12:30:01 -0700

    Create constants-EXAMPLE.py
    add constant example

---
+++ constants-EXAMPLE.py
@@ -0,0 +1,20 @@
+ALL_AMINO_ACIDS = 'ACDEFGHIKLMNPQRSTVWY'
+WILD_TYPE_STARTING_AMINO_ACIDS = {
+    122: 'A',
+    125: 'L',
+    126: 'Y',
+    129: 'M',
+    162: 'A',
+    164: 'G',
+    202: 'Y',
+    219: 'A',
+    235: 'C',
+    237: 'G',
+}
+
+# path to the wild-type PDB file
+# This is the PDB file that will be used as the input for the PyRosetta protocol
+INPUT_WT_PDB_PATH = 'wt.pdb'
+
+# path to the ligand parameters file
+LIG_PARAMS_PATH = 'LIG.params'
